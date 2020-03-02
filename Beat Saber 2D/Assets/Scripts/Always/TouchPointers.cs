216b90993be30d9f38296a3908d304bcc2db53e1
StepanFukalov <StepanFukalov@users.noreply.github.com>
2020-03-02 18:58:56 +0300

    I fixed touches, but cubes aren't destroyed with touch.

--- Beat Saber 2D/Assets/Scripts/Always/TouchPointers.cs
+++ Beat Saber 2D/Assets/Scripts/Always/TouchPointers.cs
@@ -5,44 +5,67 @@ using UnityEngine.UI;
 
 public class TouchPointers : MonoBehaviour
 {
+	[SerializeField] private Camera cam;
 	[SerializeField] private GameObject touchGameObject1, touchGameObject2;
 	[SerializeField] private Text text;
 
-	private bool touchCorrect = false;
+	private bool touchCorrect = false, redFirst = false;
 	private Touch touch1, touch2;
+	private Vector3 touch1Position, touch2Position;
+
+	private void Awake() {
+		cam.enabled = false;
+	}
 
 	private void Update() {
 		if(Input.touchCount > 1) {
-			// if(touchCorrect)
-			// 	UpdatePointersPosition();
-			// else
-			// 	UpdateTouches();
-			UpdatePointersPosition();
+				UpdateTouches();
 		} 
 		else HidePointers();
 	}
 
-	// private void UpdateTouches() {
-	// 	touch1 = Input.GetTouch(0);
-	// 	touch2 = Input.GetTouch(1);
-	// 	float xCenter = Screen.width / 2;
-	// 	if(touch1.position.x < xCenter) {
-	// 		if(touch2.position.x > xCenter) {
-	// 			touchGameObject1.SetActive(true);
-	// 			touchGameObject1.SetActive(true);
-	// 			touchCorrect = true;
-	// 			UpdatePointersPosition();
-	// 		}
-	// 		else HidePointers();
-	// 	}
-	// 	else HidePointers();
-	// }
+	private void UpdateTouches() {
+		touch1 = Input.GetTouch(0);
+		touch2 = Input.GetTouch(1);
+		touch1Position = cam.ScreenToWorldPoint(touch1.position);
+		touch2Position = cam.ScreenToWorldPoint(touch2.position);
+		if(!touchCorrect) {
+			if(touch1Position.x < 0f) {
+				if(touch2Position.x > 0f) {
+					touchGameObject1.SetActive(true);
+					touchGameObject2.SetActive(true);
+					touchCorrect = true;
+					redFirst = true;
+					UpdateTouchPositions(redFirst);
+				}
+				else HidePointers();
+			}
+			else if(touch1Position.x > 0f) {
+				if(touch2Position.x < 0f) {
+					touchGameObject1.SetActive(true);
+					touchGameObject2.SetActive(true);
+					touchCorrect = true;
+					redFirst = false;
+					UpdateTouchPositions(redFirst);
+				}
+				else HidePointers();
+			}
+			else HidePointers();
+		}
+		else {
+			UpdateTouchPositions(redFirst);
+		}
+	}
 
-	private void UpdatePointersPosition() {
-		touchGameObject1.SetActive(true);
-		touchGameObject2.SetActive(true);
-		touchGameObject1.transform.position = Camera.main.ScreenToWorldPoint(touch1.position);
-		touchGameObject2.transform.position = Camera.main.ScreenToWorldPoint(touch2.position);
+	private void UpdateTouchPositions(bool red) {
+		if(red) {
+			touchGameObject1.transform.position = new Vector3(touch1Position.x, touch1Position.y, -2f);
+			touchGameObject2.transform.position = new Vector3(touch2Position.x, touch2Position.y, -2f);
+		}
+		else {
+			touchGameObject1.transform.position = new Vector3(touch2Position.x, touch2Position.y, -2f);
+			touchGameObject2.transform.position = new Vector3(touch1Position.x, touch1Position.y, -2f);
+		}
 	}
 
 	private void HidePointers() {
---
+++ Beat Saber 2D/Assets/Scripts/Always/TrailTouch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailTouch : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+    	Debug.Log("Hit");
+    	Destroy(other.gameObject);
+    }
+}
